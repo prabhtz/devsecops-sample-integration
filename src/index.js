17f717486968ece3281dc1b0e916f1ea698a6b14
prabhtz <gunpaul41@gmail.com>
2025-03-31 21:52:03 -0400

    feat: add server file

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const url = require('url');
+const port = process.env.PORT || 3000;
+
+const requestHandler = (req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === '/echo' && parsedUrl.query.message) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ echo: parsedUrl.query.message }));
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello from DevSecOps Node.js App!');
+  }
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});

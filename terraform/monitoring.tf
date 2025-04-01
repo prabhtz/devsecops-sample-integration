5f451aa84a6b14b168179f2489d5c60806c6a3ab
prabhtz <gunpaul41@gmail.com>
2025-04-01 17:44:55 -0400

    feat: add EC2 instance for monitoring

---
+++ terraform/monitoring.tf
@@ -0,0 +1,68 @@
+# Monitoring Security Group
+resource "aws_security_group" "monitoring_sg" {
+  name        = "monitoring-sg"
+  description = "Allow Grafana (3000) and Prometheus (9090)"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Allow Prometheus UI"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    # tfsec:ignore:aws-ec2-no-public-ingress-sgr
+    cidr_blocks = "0.0.0.0/0"
+  }
+
+  ingress {
+    description = "Allow Grafana UI"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    # tfsec:ignore:aws-ec2-no-public-ingress-sgr
+    cidr_blocks = "0.0.0.0/0"
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # tfsec:ignore:aws-ec2-no-public-ingress-sgr
+    cidr_blocks = "0.0.0.0/0"
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # tfsec:ignore:aws-ec2-no-public-egress-sgr
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "MonitoringSG"
+  }
+}
+
+# Monitoring EC2 Instance
+resource "aws_instance" "monitoring_instance" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.monitoring_sg.id]
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 8
+    encrypted   = true
+  }
+
+  tags = {
+    Name        = "MonitoringServer"
+    Environment = "DevSecOps"
+  }
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -7,3 +7,8 @@ output "app_url" {
   description = "App URL"
   value       = "http://${aws_instance.devsecops_instance.public_ip}:${var.app_port}"
 }
+
+output "monitoring_instance_ip" {
+  description = "Public IP of the monitoring EC2 instance"
+  value       = aws_instance.monitoring_instance.public_ip
+}

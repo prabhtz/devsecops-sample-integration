a720792c578d500cb44b74285940e7e1a6f9fd69
prabhtz <gunpaul41@gmail.com>
2025-04-01 13:19:07 -0400

    chore: fix CIDR blocks

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "devsecops_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["192.168.1.1/24"]
+    cidr_blocks = ["192.168.1.0/24"]
   }
 
   ingress {
@@ -20,7 +20,7 @@ resource "aws_security_group" "devsecops_sg" {
     from_port   = var.app_port
     to_port     = var.app_port
     protocol    = "tcp"
-    cidr_blocks = ["192.168.1.1/24"]
+    cidr_blocks = ["192.168.1.0/24"]
   }
 
   egress {
